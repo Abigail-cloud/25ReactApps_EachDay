b0e4f183f322e2d59d96fa4c850f81ad4088eed5
Toluwalase Abigail <toluabby12@gmail.com>
2024-06-18 17:46:14 +0100

    all done

--- filter_author/src/AuthorList.jsx
+++ filter_author/src/AuthorList.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "./index.css";
+import {FaSearch} from 'react-icons/fa'
 
 const AuthorList = ({authors}) => {
  const [search, setSearch] = useState('');
@@ -16,7 +18,7 @@ const AuthorList = ({authors}) => {
       <span>
         {parts.map((part, id) =>
           part.toLowerCase() === highlight.toLowerCase() ? (
-            <span key={id} style={{ backgroundColor: 'yellow' }}>
+            <span  className="highlighted" key={id} >
               {part}
             </span>
           ) : (
@@ -27,21 +29,24 @@ const AuthorList = ({authors}) => {
     );
   };
   return (
-    <div>
-     <input type="text"
-     placeholder="search for author"
-     value={search}
-     onChange={handleSearchQuery}
-     />
-     <ul>
-      {authors.map((author)=>(
-       <li key={authors.id}>
-        {getHighlightedText(author.name, search)}
-       </li>
-      ))}
-     </ul>
+    <div >
+      <div className='search-container'>
+        <FaSearch className='search-icon' />
+        <input
+          type='text'
+          placeholder='search for author'
+          value={search}
+          onChange={handleSearchQuery}
+          className='search-bar'
+        />
+      </div>
+      <ul>
+        {authors.map((author) => (
+          <li key={authors.id}>{getHighlightedText(author.name, search)}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default AuthorList
