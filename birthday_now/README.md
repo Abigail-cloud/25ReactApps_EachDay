17c2748453d9870a16812885f8475f5bfbfd9a52
Toluwalase Abigail <toluabby12@gmail.com>
2024-06-19 16:44:18 +0100

    all updated

--- birthday_now/README.md
+++ birthday_now/README.md
@@ -1,5 +1,14 @@
-# React + Vite
+# Day 1 : People's Birthday Today: A mini react app
+- The Birthday_Now mini app: shows people whose birthday(s) is today with just basic react Js library by using the "useState" hook to get the by the id and name.
+- UseState hooks takes the initial state of a value or data as an argument and return a curent value and a function to update the value.
+- In this app it is return an array or when cleared it returns an empty array[].
 
+![Alt text](./public/B1.png "With people birthdays")
+![Alt text](./public/B2.png "With empty people's birthdays")
+
+- Design from : https://www.uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042
+
+# React + Vite
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- birthday_now/src/App.jsx
+++ birthday_now/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import PeopleList from "./PeopleList";
+import data from "./data.js";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+   const [people, setPeople] = useState(data);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <PeopleList people={people} />
+        <button onClick={()=>setPeople([])}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App
